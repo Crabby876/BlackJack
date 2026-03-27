fe0435aa1af110c29a9bf7bff8fb45ea0fedaa26
Doruk Güler <doriguler@gmail.com>
2026-03-27 15:00:06 +0100

    Deploy Azure

--- BJ-Frontend/src/socket.js
+++ BJ-Frontend/src/socket.js
@@ -1,2 +1,7 @@
 import { io } from 'socket.io-client';
-export const socket = io('http://localhost:3001/');
+
+const URL = import.meta.env.MODE === 'production'
+  ? 'https://blackjacck21-dubwbnf2eyhjd4b0.swedencentral-01.azurewebsites.net'
+  : 'http://localhost:3001';
+
+export const socket = io(URL);
