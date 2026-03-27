c2fa51dc13d418ca9bc312e0cd94531b24bc198a
Doruk Güler <doriguler@gmail.com>
2026-03-27 15:09:11 +0100

    fixed Deploy

--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,39 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://blackjacck21-dubwbnf2eyhjd4b0.swedencentral-01.azurewebsites.net'
+  ]
+}));
+
 app.use(express.json());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      'http://localhost:5173',
+      'https://blackjacck21-dubwbnf2eyhjd4b0.swedencentral-01.azurewebsites.net'
+    ],
     methods: ["GET", "POST"]
   }
 });
 
 // Routes
 app.use('/game', require('./routes/game'));
+
+// React Build servieren (nach den API-Routen!)
+app.use(express.static(path.join(__dirname, '../BJ-Frontend/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../BJ-Frontend/dist', 'index.html'));
+});
 
 // Socket
 const lobbyHandler = require('./Socket/lobbyHandler');
